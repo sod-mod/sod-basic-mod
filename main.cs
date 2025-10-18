35bcbf8a8a21ec16c6a018038ee26a5c8b59f560
Min <sod.mod.2025@gmail.com>
2025-10-18 16:39:23 +0900

    add harmony

--- main.cs
+++ main.cs
@@ -7,23 +7,27 @@ public class SodBasicMod : ModBehaviour
     private void Start()
     {
         Debug.Log("[SodBasicMod] enabled");
+        this.harmony.PatchAll();
     }
     
     private void OnDestroy()
     {
         Debug.Log("[SodBasicMod] disabled");
+        this.harmony.UnpatchAll(this.mod.metadata.id);
     }
 }
 
-[HarmonyPatch(typeof(GameManager), "OnStartServer")]
-public static class GameManager_OnStartServer_Patch
+[HarmonyPatch(typeof(ZoneManager), "OnStart")]
+public static class ZoneManager_OnStart_Patch
 {
     public static void Postfix()
     {
+        Debug.Log("[SodBasicMod] ZoneManager OnStart");
         if (DewNetworkManager.continueData == null)
         {
             NetworkedManagerBase<ZoneManager>.instance.CallOnReadyAfterTransition(delegate 
             {
+                Debug.Log("[SodBasicMod] ZoneManager OnReadyAfterTransition");
                 var player = DewPlayer.local;
                 Vector3 spawnPos = Dew.GetGoodRewardPosition(player.hero.agentPosition, 2f);
                 SkillTrigger target = DewResources.GetByShortTypeName<SkillTrigger>("St_C_Sneeze", default(VariantDef));
